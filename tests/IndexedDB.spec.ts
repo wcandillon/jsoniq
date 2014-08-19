02dc220048137568ffca57df557a347a2c587a03
William Candillon <wcandillon@gmail.com>
2014-08-19 16:19:11 +0200

    karma refactoring

---
+++ tests/IndexedDB.spec.ts
@@ -0,0 +1,15 @@
+///<reference path='../typings/jest/jest.d.ts'/>
+///<reference path='../lib/updates/PUL.ts' />
+var PUL = require("PUL");
+
+describe("PUL", () => {
+    it("Should build a simple PUL", () => {
+        var pul = new PUL();
+        pul
+            .insertIntoObject("1", [], { b: 2 })
+            .insertIntoArray("1", [], 0, ["a"])
+            .deleteFromObject("1", [], ["a", "b"])
+            .replaceInObject("1", [], "a", 1);
+        expect(true).toBe(true);
+    });
+});
