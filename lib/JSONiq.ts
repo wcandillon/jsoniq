7db240583df59b39398792a41f77ed57a39c034c
William Candillon <wcandillon@gmail.com>
2015-05-08 22:26:17 -0700

    add sourcemap support

--- lib/JSONiq.ts
+++ lib/JSONiq.ts
@@ -1,4 +1,5 @@
 /// <reference path="../typings/tsd.d.ts" />
+require('source-map-support').install();
 import Marker = require("./compiler/Marker");
 import Translator = require("./compiler/Translator");
 import Position = require("./compiler/parsers/Position");
@@ -73,7 +74,7 @@ class JSONiq {
     }
 }
 
-var jsoniq = new JSONiq("1 + 1 + 1 - 1 - 1 + 10 - 1, (1 to 5), (1, (), 2, 3), 20.1 idiv 1.678, 10 div 2, 2 * 5");
+//var jsoniq = new JSONiq("1 + 1 + 1 - 1 - 1 + 10 - 1, (1 to 5), (1, (), 2, 3), 20.1 idiv 1.678, 10 div 2, 2 * 5");
 /*
 it.forEach(item => {
    console.log(item);
@@ -83,7 +84,7 @@ it.forEach(item => {
 
 //for $a in (1 to 100) where $a le 10 for $b in (1 to 10) where $a * $b ge 50 return $a * $b
 //var jsoniq = new JSONiq("for $a in (1 to 10) for $b in (1 to 10) return $a * $b");
-//var jsoniq = new JSONiq("for $i in (2 to 5) return $i * $i");
+var jsoniq = new JSONiq("for $i in (2 to 5) return $i * $i");
 var it = jsoniq.compile();
 it.forEach(item => {
     console.log(item.get());
