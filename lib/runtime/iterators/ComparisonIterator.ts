81ade49cbfd8d0bde5ab7501419fe1a8dd283c56
William Candillon <wcandillon@gmail.com>
2015-05-13 11:17:18 -0700

    :new: comparison iterator

---
+++ lib/runtime/iterators/ComparisonIterator.ts
@@ -0,0 +1,68 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+import Iterator = require("./Iterator");
+import Position = require("../../compiler/parsers/Position");
+
+import Item = require("../items/Item");
+
+class ComparisonIterator extends Iterator {
+
+    private operator: string;
+    private left: Iterator;
+    private right: Iterator;
+
+    constructor(position: Position, left: Iterator, right: Iterator, operator: string) {
+        super(position);
+        this.left = left;
+        this.right = right;
+        this.operator = operator;
+    }
+
+    next(): Promise<Item> {
+        if(this.closed) {
+            return this.emptySequence();
+        }
+        var valueOp: (left: any, right: any) => boolean;
+        if(this.operator === "eq") {
+            valueOp = function(left, right) {
+                return left === right;
+            };
+        } else if(this.operator === "ne") {
+            valueOp = function(left, right) {
+                return left !== right;
+            };
+        } else if(this.operator === "lt") {
+            valueOp = function(left, right) {
+                return left < right;
+            };
+        } else if(this.operator === "le") {
+            valueOp = function(left, right) {
+                return left <= right;
+            };
+        } else if(this.operator === "gt") {
+            valueOp = function(left, right) {
+                return left > right;
+            };
+        } else if(this.operator === "ge") {
+            valueOp = function(left, right) {
+                return left >= right;
+            };
+        }
+        if(valueOp) {
+            this.closed = true;
+            return Promise.all([this.left.next(), this.right.next()]).then(values => {
+                var result = valueOp(values[0].get(), values[1].get());
+                return Promise.resolve(new Item(result));
+            });
+        }
+        throw new Error("Unknown operator: " + this.operator);
+    }
+
+    reset(): Iterator {
+        super.reset();
+        this.left.reset();
+        this.right.reset();
+        return this;
+    }
+};
+
+export = ComparisonIterator;
