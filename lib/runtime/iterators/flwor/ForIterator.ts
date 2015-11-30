c777ae20755caefd3e88bfe0219deaf6e058d3a7
William Candillon <wcandillon@gmail.com>
2015-11-30 13:04:59 +0100

    :lipstick:

--- lib/runtime/iterators/flwor/ForIterator.ts
+++ lib/runtime/iterators/flwor/ForIterator.ts
@@ -7,8 +7,6 @@ import Position from "../../../compiler/parsers/Position";
 
 export default class ForIterator extends IteratorClause {
 
-    private static index = 0;
-
     private varName: string;
     private allowEmpty: boolean;
     private positionalVar: string;
@@ -25,19 +23,15 @@ export default class ForIterator extends IteratorClause {
     }
 
     serializeClause(clauses: IteratorClause[]): SourceMap.SourceNode {
-        ForIterator.index++;
-        var index = ForIterator.index;
         var node = new SourceMap.SourceNode(this.position.getStartLine() + 1, this.position.getStartColumn() + 1, this.position.getFileName());
-        node.add("let $$pos" + index + " = 0;\n");
         if(this.positionalVar) {
-            node.add("let $" + this.positionalVar + " = $$pos" + index + ";\n");
+            node.add("let $" + this.positionalVar + " = 0;\n");
         }
         node.add("for(let $" + this.varName + " of ");
         node.add(this.expr.serialize());
         node.add(") {\n");
-        node.add("$$pos" + index + "++;\n");
         if(this.positionalVar) {
-            node.add("$" + this.positionalVar + " = $$pos" + index + ";\n");
+            node.add("$" + this.positionalVar + "++;\n");
         }
         node.add(
             clauses[0].serializeClause(clauses.slice(1))
