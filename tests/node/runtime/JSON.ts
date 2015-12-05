8a7fb4236bfad80fad274649925a1fce88e8ac31
William Candillon <wcandillon@gmail.com>
2015-12-05 12:14:20 +0100

    :wrench:

--- tests/node/runtime/JSON.ts
+++ tests/node/runtime/JSON.ts
@@ -4,10 +4,10 @@ import * as u from "./Utils";
 describe("JSON Support: ", () => {
 
     it("Navigation 1", () => {
-        return u.expectQuery("let $foo := { a: 1 } let $a := \"a\" return $foo.$a").toEqual([1]);
+        return u.expectQuery("let $foo := { a: 1 } let $a := \"a\" return $foo.$a", true).toEqual([1]);
     });
 
     it("Navigation 2", () => {
-        return u.expectQuery("let $foo := { a: 1 } return $foo.a").toEqual([1]);
+        return u.expectQuery("let $foo := { a: 1 } return $foo.a", true).toEqual([1]);
     });
 });
