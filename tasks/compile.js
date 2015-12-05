7278a1b3e0f5b38ed80dd35a545cc34e45349d90
William Candillon <wcandillon@gmail.com>
2015-12-05 14:27:56 +0100

    :wrench:

--- tasks/compile.js
+++ tasks/compile.js
@@ -6,19 +6,11 @@ var merge = require('merge2');
 
 var Config = require('./config');
 
-var test = $.typescript.createProject({
+var ts = $.typescript.createProject({
     declarationFiles: false,
     target: 'ES6',
     module: 'commonjs',
     typescript: require('typescript')
-});
-
-var dist = $.typescript.createProject({
-    declarationFiles: false,
-    target: 'ES6',
-    module: 'commonjs',
-    typescript: require('typescript'),
-    outFile: 'jsoniq.js'
 });
 
 gulp.task('compile:clean', function () {
@@ -28,19 +20,10 @@ gulp.task('compile:clean', function () {
 gulp.task('compile:typescript', ['compile:clean'], function() {
     var tsResult = gulp.src(Config.ts.concat(['typings/**/*.ts']))
         .pipe($.sourcemaps.init())
-        .pipe($.typescript(test));
+        .pipe($.typescript(ts));
     return merge([
         tsResult.dts.pipe(gulp.dest(Config.dist)),
         tsResult.js.pipe($.sourcemaps.write()).pipe(gulp.dest(Config.dist))
-    ]);
-});
-
-gulp.task('compile:dist', ['compile:typescript'], function(){
-    var tsResult = gulp.src(Config.ts.concat(['typings/**/*.ts']))
-        .pipe($.typescript(dist));
-    return merge([
-        tsResult.dts.pipe(gulp.dest(Config.dist)),
-        tsResult.js.pipe(gulp.dest(Config.dist))
     ]);
 });
 
