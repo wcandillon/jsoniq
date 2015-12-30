7cc9c0382a2720a923ef49a92b5a62239866f214
William Candillon <w@28.io>
2015-12-30 09:39:28 +0100

    Update tests.js

--- tasks/tests.js
+++ tasks/tests.js
@@ -5,5 +5,7 @@ var $ = require('gulp-load-plugins')();
 
 gulp.task('tests:node', function () {
     var jasmine = require('gulp-jasmine');
-    return gulp.src('dist/tests/node/runtime/*.js').pipe(jasmine());
+    return gulp.src('dist/tests/node/runtime/*.js').pipe(jasmine({
+        includeStackTrace: true
+    }));
 });
